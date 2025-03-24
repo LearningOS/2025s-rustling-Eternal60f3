5ad9ce35384d4151b869fede98bea8dbc9164abe
dream <dreamveid060@gmail.com>
2025-03-24 10:12:26 +0800

    finish 71

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -39,6 +37,25 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        let mut func = |team_name: &str, self_score: u8, opponent_score| {
+            if scores.contains_key(team_name) {
+                let team = scores.get_mut(team_name).unwrap();
+                team.goals_scored += self_score;
+                team.goals_conceded += opponent_score;
+            } else {
+                scores.insert(
+                    team_name.to_owned(),
+                    Team {
+                        goals_scored: self_score,
+                        goals_conceded: opponent_score,
+                    },
+                );
+            }
+        };
+
+        func(&team_1_name, team_1_score, team_2_score);
+        func(&team_2_name, team_2_score, team_1_score);
     }
     scores
 }
